c9b5db3409ad00f7e81e2a7f9bd9c1bf67b95b9f
Naty <natyfrie@gmail.com>
2016-05-24 19:54:15 +0300

    This is working version!

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,48 @@
-var myImage = document.querySelector('img');
+$(document).ready(
+  function(){
+    addMatrix(4,4,90);
+    makeBlue();
+  }
+);
 
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/dog.jpeg') {
-      myImage.setAttribute ('src','images/cat.jpeg');
-    } else {
-      myImage.setAttribute ('src','images/dog.jpeg');
+
+var addMatrix = function(numRows, nowCols, size){
+  //Total size should be 90*4 = 360
+
+  for(var i = 0; i < numRows; i++){
+    $('#top_div').append("<div id = 'row" + i +"'></div>"); //creating new row
+    //console.log("Created new row:" + i);
+    
+    for(var j = 0; j < nowCols; j++){ //creating 4 boxes
+      $("#row" + i).append("<div class = 'box'></div>");
+      //console.log("Created new box, j: " + j);
     }
-}
+  }
+  $(".box").height(size);
+  $(".box").width(size);
+};
 
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
+var makeBlue = function(){
+  $(".box").mouseenter(function(){
+    $(this).addClass("blue_box");
+  });
+};
 
-function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is uncool, ' + myName;
-}
+var buttonClick = function(){
+  $(".box").removeClass("blue_box");
+  numOfSquares = prompt("How many squares do you want on the side?");
+  console.log(numOfSquares);
 
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is uncool, ' + storedName;
-}
+  if (isNaN(numOfSquares)){
+    console.log("Not a number! Idiot!");
+  }
+  else{
+    //removing current matrix and readding top_div
+    $("#top_div").remove();
+    $("body").append("<div id='top_div'></div>");
 
-myButton.onclick = function() {
-  setUserName();
-}
+    //creating new matrix
+    addMatrix(numOfSquares,numOfSquares,Math.round(360/numOfSquares));
+    makeBlue();
+  }
+};
